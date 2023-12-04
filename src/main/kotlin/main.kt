170d2df0c7b1618807368295911c72b231b95af1
Peter Tamas <peter.tamas@getbridge.com>
2023-12-04 08:28:52 +0100

    add main with examples

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,19 @@
+import dec_04.part1
+import dec_04.part2
+import java.io.File
+
+fun main() {
+    val day = "04"
+    val example = File("src/main/kotlin/dec_$day/example.txt").readLines()
+    val input = File("src/main/kotlin/dec_$day/input.txt").readLines()
+
+    println("Part 1")
+    println("* Example: ${part1(example)}")
+    println("* Input: ${part1(input)}")
+
+    println()
+
+    println("Part 2")
+    println("* Example: ${part2(example)}")
+    println("* Input: ${part2(input)}")
+}
