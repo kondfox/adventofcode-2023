92feb62ef26ca7f96e413d69664807ebdccf09e5
Peter Tamas <peter.tamas@getbridge.com>
2023-12-04 07:53:59 +0100

    feat(dec_03): solve part 2

--- src/main/kotlin/dec_04/Dec04.kt
+++ src/main/kotlin/dec_04/Dec04.kt
@@ -14,3 +14,24 @@ fun part1(input: List<String>): Int {
          }
     }
 }
+
+/* Part 2 */
+
+fun part2(input: List<String>): Int {
+    return input.indices.fold(mutableMapOf<Int, Int>()) { cardCounts, i ->
+        val (winnerNumbers, myNumbers) = input[i].split(": ")[1]
+            .split(" | ")
+            .map { it.trim().split(Regex("[ ]+")).toSet() }
+        myNumbers.count { it in winnerNumbers }.let {
+            ((i + 1)..(i + it)).forEach { cardIndex ->
+                if (cardIndex in input.indices) {
+                    val actual = cardCounts[i] ?: 1
+                    cardCounts[cardIndex] = cardCounts[cardIndex]?.plus(actual) ?: (actual + 1)
+                }
+            }
+        }
+        cardCounts
+    }.let { cardCounts ->
+        cardCounts.values.sum() + input.size - cardCounts.size
+    }
+}
