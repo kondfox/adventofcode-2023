d47b6cfb4824fd953042cb8211a0466061919c21
Peter Tamas <peter.tamas@greenfoxacademy.com>
2023-12-04 13:53:08 +0100

    refactor(dec_03): refactor solution

--- src/main/kotlin/dec_03/Dec03.kt
+++ src/main/kotlin/dec_03/Dec03.kt
@@ -2,85 +2,60 @@ package dec_03
 
 /* Part 1 */
 
-fun part1(input: List<String>): Int {
-    return input.joinToString("").let { engine ->
-        collectNumbersWithIndices(engine).filter {
-            isPart(it, engine, input[0].length, Regex("[^0-9\\.]+"))
-        }.values.sumOf { it.toInt() }
-    }
+fun part1(input: List<String>): Int = input.joinToString("").let { engine ->
+    parseParts(engine, input[0].length, Regex("[^0-9\\.]+")).sumOf { it.num.toInt() }
 }
 
-private fun collectNumbersWithIndices(engine: String): Map<Int, String> {
-    val numbers = mutableMapOf<Int, String>()
-    var num = ""
-    var numIdx = 0
-    engine.indices.forEach { i ->
+private fun parseParts(engine: String, width: Int, rule: Regex): List<NumPosition> =
+        parseNumPositions(engine, width).filter { isPart(it, engine, rule) }
+
+private fun parseNumPositions(engine: String, width: Int): List<NumPosition> = mutableListOf<NumPosition>().also {
+    var i = 0
+    while (i < engine.length) {
         if (engine[i].isDigit()) {
-            if (num.isEmpty()) {
-                numIdx = i
+            parseNumber(engine, i).let { num ->
+                it.add(NumPosition(i, num, width))
+                i += num.length
             }
-            num += engine[i]
-        } else if (num.isNotEmpty()) {
-            numbers[numIdx] = num
-            num = ""
-            numIdx = i
+        } else {
+            i++
         }
     }
-    return numbers
 }
 
-private fun isPart(indexWithNum: Map.Entry<Int, String>, engine: String, width: Int, regex: Regex): Boolean {
-    val (i, num) = indexWithNum
-    return when {
-        regex.matches(engine.pos(leftOf(i))) -> true
-        regex.matches(engine.pos(rightOf(i + num.length - 1))) -> true
-        (-1..num.length).any() { regex.matches(engine.pos(aboveOf(i + it, width))) } -> true
-        (-1..num.length).any() { regex.matches(engine.pos(belowOf(i + it, width))) } -> true
-        else -> false
-    }
+data class NumPosition(val i: Int, val num: String, val width: Int) {
+    private fun left() = if (i % width == 0) i else i - 1
+    private fun right() = (i + num.length).let { if ((it % width) == width - 1) it - 1 else it }
+    private fun allAbove() = ((left() - width)..(right() - width)).map { it }
+    private fun allBelow() = ((left() + width)..(right() + width)).map { it }
+    fun perimeter() = allAbove() + listOf(left(), right()) + allBelow()
 }
 
-private fun leftOf(i: Int) = i - 1
-private fun rightOf(i: Int) = i + 1
-private fun aboveOf(i: Int, width: Int) = i - width
-private fun belowOf(i: Int, width: Int) = i + width
+private fun parseNumber(engine: String, i: Int): String =
+        engine.substring(i, i + engine.drop(i).indexOfFirst { Regex("[^0-9]").matches(it.toString()) })
+
+private fun isPart(p: NumPosition, engine: String, rule: Regex): Boolean =
+        p.perimeter().any { rule.matches(engine.pos(it)) }
 
 private fun String.pos(i: Int): String = if (i in this.indices) this[i].toString() else ""
 
 /* Part 2 */
 
-fun part2(input: List<String>): Int {
-    return input.joinToString("").let { engine ->
-        val parts = collectNumbersWithIndices(engine).filter {
-            isPart(it, engine, input[0].length, Regex("[\\*]+"))
-        }
-        val gearConnections = parts.map { connection(engine, it, input[0].length) }
-                .groupingBy { it }.eachCount()
-                .filter { it.value == 2 }
-                .keys
-        val gears = gearConnections.associate {
-            it to parts.filter { part -> connection(engine, part, input[0].length) == it }
-        }
-        return gears.values.sumOf { gear ->
-            gear.map { it.value.toInt() }.reduce(Int::times)
-        }
-    }
+fun part2(input: List<String>): Int = input.joinToString("").let { engine ->
+    parseParts(engine, input[0].length, Regex("[\\*]+"))
+            .findGears(engine)
+            .sumOf { gear ->
+                gear.parts.map { it.num.toInt() }.reduce(Int::times)
+            }
 }
 
-private fun connection(engine: String, part: Map.Entry<Int, String>, width: Int, symbol: String? = "*"): Int {
-    val (i, num) = part
-    return when {
-        engine.pos(leftOf(i)) == symbol -> i - 1
-        engine.pos(rightOf(i + num.length - 1)) == symbol -> i + num.length
-        (-1..num.length).any { engine.pos(aboveOf(i + it, width)) == symbol } -> {
-            return (-1..num.length).first { engine.pos(aboveOf(i + it, width)) == symbol }.let {
-                aboveOf(i + it, width)
-            }
-        }
-        else -> {
-            return (-1..num.length).first { engine.pos(belowOf(i + it, width)) == symbol }.let {
-                belowOf(i + it, width)
-            }
-        }
-    }
-}
+private fun List<NumPosition>.findGears(engine: String): List<Gear> = this.map { part ->
+    connection(engine, part) to part
+}.groupBy({ it.first }, { it.second })
+    .map { Gear(it.key, it.value) }
+    .filter { it.parts.size == 2 }
+
+private fun connection(engine: String, part: NumPosition, symbol: String? = "*"): Int =
+    part.perimeter().first { engine.pos(it) == symbol }
+
+data class Gear(val connection: Int, val parts: List<NumPosition>)
