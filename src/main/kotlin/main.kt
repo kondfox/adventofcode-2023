2d3f62274297637159257b0093eaa7493a2084bb
Peter Tamas <peter.tamas@getbridge.com>
2023-12-05 20:05:51 +0100

    feat(main): add execution time measurement

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,7 @@
 import dec_05.part1
 import dec_05.part2
 import java.io.File
+import kotlin.time.measureTimedValue
 
 fun main() {
     val day = "05"
@@ -8,12 +9,17 @@ fun main() {
     val input = File("src/main/kotlin/dec_$day/input.txt").readLines()
 
     println("Part 1")
-    println("* Example: ${part1(example)}")
-    println("* Input: ${part1(input)}")
+    println("* Example: ${solve { part1(example) }}")
+    println("* Input: ${solve { part1(input) }}")
 
     println()
 
     println("Part 2")
-    println("* Example: ${part2(example)}")
-    println("* Input: ${part2(input)}")
+    println("* Example: ${solve { part2(example) }}")
+    println("* Input: ${solve { part2(input) }}")
+}
+
+fun solve(function: () -> Any): String {
+    val (result, time) = measureTimedValue(function)
+    return "$result [${time}]"
 }
