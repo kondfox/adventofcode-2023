8231ad8e905ef520d60a5c00d3aeb54f47dec080
Peter Tamas <peter.tamas@getbridge.com>
2023-12-06 17:09:32 +0100

    refactor(dec_06): remove parsing

--- src/main/kotlin/dec_06/Dec06.kt
+++ src/main/kotlin/dec_06/Dec06.kt
@@ -1,33 +1,15 @@
 package dec_06
 
-import kotlin.math.ceil
-import kotlin.math.floor
-import kotlin.math.sqrt
+fun part1() = solve(listOf(7L, 15L, 30L).zip(listOf(9L, 40L, 200L)))
 
-/* Part 1 */
-
-fun part1(input: List<String>) = solve(parse1(input))
-
-private fun parse1(input: List<String>) = parse(input)
-    .map { it.map { it.value.toLong() }.toList() }.toList().let { (time, distance) -> time.zip(distance) }
-
-private fun parse(input: List<String>) = input.windowed(2)
-    .flatMap { it.map { it.split(":")[1].trim() } }
-    .map { Regex("[0-9]+").findAll(it) }
+fun part2() = solve(listOf(71530L to 940200L))
 
 private fun solve(races: List<Pair<Long, Long>>) = races
     .map { (time, distance) -> quadratic(1, -time, distance) }
-    .map { ceil(it.second).toLong() - floor(it.first).toLong() - 1 }
+    .map { it.second - it.first - 1 }
     .reduce(Long::times)
 
-private fun quadratic(a: Int, b: Long, c: Long): Pair<Double, Double> {
-    val d = sqrt(b.toDouble() * b - 4 * a * c)
-    return ((-b - d) / (2 * a)) to ((-b + d) / (2 * a))
+private fun quadratic(a: Int, b: Long, c: Long): Pair<Long, Long> {
+    val d = Math.sqrt(b.toDouble() * b - 4 * a * c)
+    return Math.floor((-b - d) / (2 * a)).toLong() to Math.ceil((-b + d) / (2 * a)).toLong()
 }
-
-/* Part 2 */
-
-fun part2(input: List<String>) = solve(parse2(input))
-
-private fun parse2(input: List<String>) = parse(input)
-    .map { it.fold("") { num, n -> num + n.value }.toLong() }.zipWithNext()
