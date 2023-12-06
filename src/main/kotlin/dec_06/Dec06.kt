c68576eea8a002da0b8687af4fcacf0d3045852b
Peter Tamas <peter.tamas@getbridge.com>
2023-12-06 12:51:49 +0100

    feat(dec_06): solve part 2

--- src/main/kotlin/dec_06/Dec06.kt
+++ src/main/kotlin/dec_06/Dec06.kt
@@ -2,21 +2,21 @@ package dec_06
 
 /* Part 1 */
 
-fun part1(input: List<String>): Int {
-    return parse(input)
-        .map { (time, distance) -> quadratic(1, -time, distance).correctEquals() }
-        .map { Math.ceil(it.second).toInt() - Math.ceil(it.first).toInt() }
-        .reduce(Int::times)
-}
+fun part1(input: List<String>) = solve(parse(input))
 
-private fun parse(input: List<String>): List<Pair<Int, Int>> {
+private fun parse(input: List<String>): List<Pair<Long, Long>> {
     val (time, distance) = input.windowed(2)
         .flatMap { it.map { it.split(":")[1].trim() } }
-        .map { Regex("[0-9]+").findAll(it).map { it.value.toInt() }.toList() }.toList()
+        .map { Regex("[0-9]+").findAll(it).map { it.value.toLong() }.toList() }.toList()
     return time.zip(distance)
 }
 
-private fun quadratic(a: Int, b: Int, c: Int): Pair<Double, Double> {
+private fun solve(races: List<Pair<Long, Long>>) = races
+    .map { (time, distance) -> quadratic(1, -time, distance).correctEquals() }
+    .map { Math.ceil(it.second).toLong() - Math.ceil(it.first).toLong() }
+    .reduce(Long::times)
+
+private fun quadratic(a: Int, b: Long, c: Long): Pair<Double, Double> {
     val d = Math.sqrt(b.toDouble() * b - 4 * a * c)
     return ((-b - d) / (2 * a)) to ((-b + d) / (2 * a))
 }
@@ -24,4 +24,15 @@ private fun quadratic(a: Int, b: Int, c: Int): Pair<Double, Double> {
 private fun Pair<Double, Double>.correctEquals() = when {
     (first * 10) % 10 == 0.0 -> first + 1 to second - 0.99
     else -> this
+}
+
+/* Part 2 */
+
+fun part2(input: List<String>) = solve(parse2(input))
+
+private fun parse2(input: List<String>): List<Pair<Long, Long>> {
+    return input.windowed(2)
+        .flatMap { it.map { it.split(":")[1].trim() } }
+        .map { Regex("[0-9]+").findAll(it).fold("") { num, n -> num + n.value }.toLong() }
+        .zipWithNext()
 }
