2086d440de0a04c3e8e727c84c2568b1e627434e
Peter Tamas <peter.tamas@getbridge.com>
2023-12-06 16:37:58 +0100

    refactor(dec_06): remove correctEquals function

--- src/main/kotlin/dec_06/Dec06.kt
+++ src/main/kotlin/dec_06/Dec06.kt
@@ -1,38 +1,33 @@
 package dec_06
 
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.sqrt
+
 /* Part 1 */
 
-fun part1(input: List<String>) = solve(parse(input))
+fun part1(input: List<String>) = solve(parse1(input))
 
-private fun parse(input: List<String>): List<Pair<Long, Long>> {
-    val (time, distance) = input.windowed(2)
-        .flatMap { it.map { it.split(":")[1].trim() } }
-        .map { Regex("[0-9]+").findAll(it).map { it.value.toLong() }.toList() }.toList()
-    return time.zip(distance)
-}
+private fun parse1(input: List<String>) = parse(input)
+    .map { it.map { it.value.toLong() }.toList() }.toList().let { (time, distance) -> time.zip(distance) }
+
+private fun parse(input: List<String>) = input.windowed(2)
+    .flatMap { it.map { it.split(":")[1].trim() } }
+    .map { Regex("[0-9]+").findAll(it) }
 
 private fun solve(races: List<Pair<Long, Long>>) = races
-    .map { (time, distance) -> quadratic(1, -time, distance).correctEquals() }
-    .map { Math.ceil(it.second).toLong() - Math.ceil(it.first).toLong() }
+    .map { (time, distance) -> quadratic(1, -time, distance) }
+    .map { ceil(it.second).toLong() - floor(it.first).toLong() - 1 }
     .reduce(Long::times)
 
 private fun quadratic(a: Int, b: Long, c: Long): Pair<Double, Double> {
-    val d = Math.sqrt(b.toDouble() * b - 4 * a * c)
+    val d = sqrt(b.toDouble() * b - 4 * a * c)
     return ((-b - d) / (2 * a)) to ((-b + d) / (2 * a))
-}
-
-private fun Pair<Double, Double>.correctEquals() = when {
-    (first * 10) % 10 == 0.0 -> first + 1 to second - 0.99
-    else -> this
 }
 
 /* Part 2 */
 
 fun part2(input: List<String>) = solve(parse2(input))
 
-private fun parse2(input: List<String>): List<Pair<Long, Long>> {
-    return input.windowed(2)
-        .flatMap { it.map { it.split(":")[1].trim() } }
-        .map { Regex("[0-9]+").findAll(it).fold("") { num, n -> num + n.value }.toLong() }
-        .zipWithNext()
-}
+private fun parse2(input: List<String>) = parse(input)
+    .map { it.fold("") { num, n -> num + n.value }.toLong() }.zipWithNext()
