a73eac88ab1184aa5caf24af8b89d3158cc71a26
Peter Tamas <peter.tamas@getbridge.com>
2023-12-11 15:35:01 +0100

    refactor(dec10): refactor solution

--- src/main/kotlin/dec_10/Dec10.kt
+++ src/main/kotlin/dec_10/Dec10.kt
@@ -2,16 +2,23 @@ package dec_10
 
 /* Part 1 */
 
-fun part1(input: List<String>): Int {
-    val nodes = parse(input)
-    val start = nodes.firstNotNullOf { it.firstOrNull { it.pipe == 'S' } }
-    val neighbours: List<Node> = neighbours(start, nodes)
-    val loop = findLoop(nodes, neighbours.first())
-    return Math.ceil(loop.size / 2.0).toInt()
-}
+fun part1(input: List<String>): Int = Math.ceil(findLoop(parse(input)).size / 2.0).toInt()
 
 private fun parse(input: List<String>): List<List<Node>> = input.mapIndexed { y, line ->
     line.toCharArray().mapIndexed { x, pipe -> Node(x, y, pipe) }
+}
+
+private fun findLoop(nodes: List<List<Node>>): List<Node> {
+    val start = nodes.firstNotNullOf { it.firstOrNull { it.pipe == 'S' } }
+    val first = neighbours(start, nodes).first()
+    val visited = mutableListOf(start, first)
+    var actual = first
+    while (true) {
+        val neighbours = neighbours(actual, nodes)
+        val next = neighbours.firstOrNull { !visited.contains(it) } ?: return visited
+        visited.add(next)
+        actual = next
+    }
 }
 
 data class Node(val x: Int, val y: Int, val pipe: Char) {
@@ -29,36 +36,18 @@ data class Node(val x: Int, val y: Int, val pipe: Char) {
     fun reachableFrom(other: Node): Boolean = this.connections().contains(other.y to other.x)
 }
 
-private fun neighbours(node: Node, nodes: List<List<Node>>): List<Node> {
-    val connections = node.connections()
-    val notNulls = connections.filter { (y, x) -> nodes.getOrNull(y)?.getOrNull(x) != null }
-    val neighbours = notNulls.map { (y, x) -> nodes[y][x] }
-    val valids = neighbours.filter { it.reachableFrom(node) && it.pipe != 'S' }
-    return valids
-}
-
-private fun findLoop(nodes: List<List<Node>>, start: Node): List<Node> {
-    val visited = mutableListOf(start)
-    var actual = start
-    while (true) {
-        val neighbours = neighbours(actual, nodes)
-        val next = neighbours.firstOrNull { !visited.contains(it) } ?: return visited
-        visited.add(next)
-        actual = next
-    }
-}
+private fun neighbours(node: Node, nodes: List<List<Node>>): List<Node> = node.connections()
+    .filter { (y, x) -> nodes.getOrNull(y)?.getOrNull(x) != null }
+    .map { (y, x) -> nodes[y][x] }
+    .filter { it.reachableFrom(node) && it.pipe != 'S' }
 
 /* Part 2 */
 
 fun part2(input: List<String>): Int {
     val nodes = parse(input)
-    val start = nodes.firstNotNullOf { it.firstOrNull { it.pipe == 'S' } }
-    val neighbours: List<Node> = neighbours(start, nodes)
-    val loop = listOf(start) + findLoop(nodes, neighbours.first())
+    val loop = findLoop(nodes)
     val doubleNodes = doubleNodes(nodes)
-    val doubleStart = doubleNodes.firstNotNullOf { it.firstOrNull { it.pipe == 'S' } }
-    val doubleNeighbours: List<Node> = neighbours(doubleStart, doubleNodes)
-    val doubleLoop = listOf(doubleStart) + findLoop(doubleNodes, doubleNeighbours.first())
+    val doubleLoop = findLoop(doubleNodes)
     val firstInLoop = findFirstInLoop(doubleLoop)
     val areaTiles: Set<Node> = firstInLoop
         .map { findAreaTiles(doubleNodes, doubleLoop, it) }
