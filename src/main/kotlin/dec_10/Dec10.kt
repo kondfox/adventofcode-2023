c4e8b6a7cee11d0d05d4bbcc8c6b762220775df2
Peter Tamas <peter.tamas@getbridge.com>
2023-12-11 13:35:20 +0100

    feat(dec_10): solve part 2

--- src/main/kotlin/dec_10/Dec10.kt
+++ src/main/kotlin/dec_10/Dec10.kt
@@ -47,3 +47,105 @@ private fun findLoop(nodes: List<List<Node>>, start: Node): List<Node> {
         actual = next
     }
 }
+
+/* Part 2 */
+
+fun part2(input: List<String>): Int {
+    val nodes = parse(input)
+    val start = nodes.firstNotNullOf { it.firstOrNull { it.pipe == 'S' } }
+    val neighbours: List<Node> = neighbours(start, nodes)
+    val loop = listOf(start) + findLoop(nodes, neighbours.first())
+    val doubleNodes = doubleNodes(nodes)
+    val doubleStart = doubleNodes.firstNotNullOf { it.firstOrNull { it.pipe == 'S' } }
+    val doubleNeighbours: List<Node> = neighbours(doubleStart, doubleNodes)
+    val doubleLoop = listOf(doubleStart) + findLoop(doubleNodes, doubleNeighbours.first())
+    val firstInLoop = findFirstInLoop(doubleLoop)
+    val areaTiles: Set<Node> = firstInLoop
+        .map { findAreaTiles(doubleNodes, doubleLoop, it) }
+        .first { !it.any { node -> node.x == 0 || node.y == 0 || node.x == doubleNodes[0].lastIndex || node.y == doubleNodes.lastIndex } }
+    val realAreaTiles = areaTiles
+        .filter { it.x % 2 == 0 && it.y % 2 == 0 }
+        .map { nodes[it.y / 2][it.x / 2] }
+        .filter { !loop.contains(it) }
+    print(nodes, loop, realAreaTiles) // because it's beautiful <3
+    return realAreaTiles.size
+}
+
+fun findAreaTiles(nodes: List<List<Node>>, loop: List<Node>, firstInLoop: Pair<Int, Int>): Set<Node> {
+    val areaTiles = mutableSetOf<Node>()
+    val queue = mutableListOf(nodes[firstInLoop.first][firstInLoop.second])
+    while (queue.isNotEmpty()) {
+        val node = queue.removeFirst()
+        areaTiles.add(node)
+        val newAreaTiles = neighbourCoordinates().filter { (y, x) ->
+            nodes.getOrNull(node.y + y)?.getOrNull(node.x + x).let {
+                it != null && !areaTiles.contains(it) && !loop.contains(it) && !queue.contains(it)
+            }
+        }.map { (y, x) -> nodes[node.y + y][node.x + x] }
+        queue.addAll(newAreaTiles)
+    }
+    return areaTiles
+}
+
+fun findFirstInLoop(loop: List<Node>): List<Pair<Int, Int>> {
+    val start = loop.first()
+    val first = loop[1]
+    val end = loop[loop.lastIndex]
+    val y = listOf(first, end).first { it.y != start.y }.y
+    val x = listOf(first, end).first { it.x != start.x }.x
+    val diffY = y - start.y
+    val diffX = x - start.x
+    return listOf(start.y + diffY to start.x + diffX, start.y + diffY to start.x - diffX)
+}
+
+private fun doubleNodes(nodes: List<List<Node>>): List<List<Node>> {
+    val doubleNodes = (0..<nodes.size * 2).map { y ->
+        (0..<nodes[0].size * 2).map { x ->
+            if (y % 2 == 0 && x % 2 == 0) Node(x, y, nodes[y / 2][x / 2].pipe)
+            else Node(x, y, ' ')
+        }
+    }
+    return doubleNodes.mapIndexed { y, row ->
+        row.mapIndexed { x, node ->
+            if (y % 2 == 0) {
+                if (x == 0 || x >= row.lastIndex) node
+                else if (row[x - 1].pipe == '-' || row[x + 1].pipe == '-') {
+                    Node(x, y, '-')
+                } else if (row[x - 1].pipe == 'F' && setOf('7', 'J', 'S').contains(row[x + 1].pipe)) {
+                    Node(x, y, '-')
+                } else if (row[x - 1].pipe == 'L' && setOf('7', 'J', 'S').contains(row[x + 1].pipe)) {
+                    Node(x, y, '-')
+                } else if (row[x - 1].pipe == 'S' && setOf('7', 'J').contains(row[x + 1].pipe)) {
+                    Node(x, y, '-')
+                } else node
+            } else {
+                if (doubleNodes[y - 1][x].pipe == '|' || y < doubleNodes.lastIndex && doubleNodes[y + 1][x].pipe == '|') {
+                    Node(x, y, '|')
+                } else if (doubleNodes[y - 1][x].pipe == '7' && y < doubleNodes.lastIndex && setOf('J', 'L', 'S').contains(doubleNodes[y + 1][x].pipe)) {
+                    Node(x, y, '|')
+                } else if (doubleNodes[y - 1][x].pipe == 'F' && y < doubleNodes.lastIndex && setOf('J', 'L', 'S').contains(doubleNodes[y + 1][x].pipe)) {
+                    Node(x, y, '|')
+                } else if (doubleNodes[y - 1][x].pipe == 'S' && y < doubleNodes.lastIndex && setOf('J', 'L').contains(doubleNodes[y + 1][x].pipe)) {
+                    Node(x, y, '|')
+                } else node
+
+            }
+        }
+    }
+}
+
+private fun neighbourCoordinates(): List<Pair<Int, Int>> = listOf(1 to 0, -1 to 0, 0 to 1, 0 to -1)
+
+private fun print(nodes: List<List<Node>>, loop: List<Node>, realAreaTiles: List<Node>) {
+    val areaColor = "\u001b[31m" // red
+    val loopColor = "\u001b[34m" // blue
+    val reset = "\u001b[0m"
+    nodes.forEach { line ->
+        line.forEach { n ->
+            if (loop.contains(n)) print("$loopColor${n.pipe}$reset")
+            else if (realAreaTiles.contains(n)) print("${areaColor}${n.pipe}$reset")
+            else print(n.pipe)
+        }
+        println()
+    }
+}
