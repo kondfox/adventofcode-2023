b3fc3eefc0b3d8e4f20221748d7991d96543be1c
Peter Tamas <peter.tamas@getbridge.com>
2023-12-15 19:24:01 +0100

    feat(dec_15): solve part 2

--- src/main/kotlin/dec_15/Dec15.kt
+++ src/main/kotlin/dec_15/Dec15.kt
@@ -1,8 +1,37 @@
 package dec_15
 
+import java.util.Deque
+
 /* Part 1 */
 
-fun part1(input: List<String>): Int = input[0].split(",").sumOf(::hash)
+fun part1(input: String): Int = input.split(",").sumOf(::hash)
 
 private fun hash(s: String): Int =
     s.toCharArray().map(Char::code).fold(0) { hash, c -> ((hash + c) * 17) % 256}
+
+/* Part 2 */
+fun part2(input: String): Int {
+    val boxes = MutableList(256) { ArrayDeque<String>() }
+    input.split(",").forEach { instruction ->
+        val op = instruction.first { it == '=' || it == '-' }
+        val (label, focal) = instruction.split(op)
+        val box = boxes[hash(label)]
+        val existing = box.indexOfFirst { it.split(" ")[0] == label }
+        when (op) {
+            '=' -> {
+                if (existing == -1) {
+                    box.addLast("$label $focal")
+                } else {
+                    box[existing] = "$label $focal"
+                }
+            }
+            '-' -> if (existing > -1) box.removeAt(existing)
+        }
+    }
+    return boxes.mapIndexed { i, box ->
+        box.mapIndexed { j, lens ->
+            val res = (i + 1) * (j + 1) * lens.split(" ")[1].toInt()
+            res
+        }.sum()
+    }.sum()
+}
