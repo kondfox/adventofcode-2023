d24ebb3959a41dd6b2b3bda3611ea0e8d07d5dea
Peter Tamas <peter.tamas@greenfoxacademy.com>
2023-12-17 17:20:30 +0100

    feat(dec_16): solve part 2

--- src/main/kotlin/dec_16/Dec16.kt
+++ src/main/kotlin/dec_16/Dec16.kt
@@ -1,19 +1,20 @@
 package dec_16
 
+import kotlin.math.max
+
 /* Part 1 */
 
 fun part1(input: List<String>): Int {
     val fields = parse(input)
-    energize(fields, Direction.RIGHT to fields[0][0])
-    return fields.flatten().count { it.energized.any { it.value } }
+    return countEnergized(energize(fields, Direction.RIGHT to Coordinate(0, 0)))
 }
 
 private fun parse(input: List<String>): List<List<Field>> = input.mapIndexed { y, row ->
     row.toCharArray().mapIndexed { x, c -> Field(y, x, fieldType(c)) }
 }
 
-private fun energize(fields: List<List<Field>>, start: Pair<Direction, Field>) {
-    var toEnergize = mutableListOf(start)
+private fun energize(fields: List<List<Field>>, start: Pair<Direction, Coordinate>): List<List<Field>> {
+    var toEnergize = mutableListOf(start.first to fields[start.second.y][start.second.x])
     while (toEnergize.isNotEmpty()) {
         val (dir, field) = toEnergize.first()
         toEnergize = toEnergize.drop(1).toMutableList()
@@ -24,10 +25,13 @@ private fun energize(fields: List<List<Field>>, start: Pair<Direction, Field>) {
             .map { (d, f)  -> d to f!!}
         toEnergize.addAll(nexts)
     }
+    return fields
 }
 
 data class Field(val y: Int, val x: Int, val type: FieldType,
                  val energized: MutableMap<Direction, Boolean> = defaultVisitedFrom())
+
+private fun Field.clone(): Field = Field(y, x, type, energized.toMutableMap())
 
 private fun List<List<Field>>.next(from: Field, dir: Direction): Field? =
     this.getOrNull(from.y + dir.y)?.getOrNull(from.x + dir.x)
@@ -78,3 +82,26 @@ enum class FieldType(val sign: Char, var beamsTo: Map<Direction, Set<Direction>>
 enum class Direction(val y: Int, val x: Int) {
     UP(-1, 0), RIGHT(0, 1), DOWN(1, 0), LEFT(0, -1)
 }
+
+/* Part 2 */
+
+fun part2(input: List<String>): Int {
+    val fields = parse(input)
+    val maxEnergized = (0..fields.lastIndex).maxOf { y -> max(
+        countEnergized(energize(clone(fields), Direction.RIGHT to Coordinate(y, 0))),
+        countEnergized(energize(clone(fields), Direction.LEFT to Coordinate(y, fields[y].lastIndex)))
+    )}
+    return max(maxEnergized,
+        (0..fields[0].lastIndex).maxOf { x -> max(
+            countEnergized(energize(clone(fields), Direction.DOWN to Coordinate(0, x))),
+            countEnergized(energize(clone(fields), Direction.UP to Coordinate(fields.lastIndex, x)))
+        )}
+    )
+}
+
+private fun countEnergized(fields: List<List<Field>>): Int =
+    fields.flatten().count { it.energized.any { it.value } }
+
+private fun clone(fields: List<List<Field>>): List<List<Field>> = fields.map { it.map(Field::clone) }
+
+data class Coordinate(val y: Int, val x: Int)
