963db41cfb85032b0dcbbad14d1a9bf6ceae9b0c
Peter Tamas <peter.tamas@greenfoxacademy.com>
2023-12-19 09:39:01 +0100

    feat(dec_17): solve part 2

--- src/main/kotlin/dec_17/Dec17.kt
+++ src/main/kotlin/dec_17/Dec17.kt
@@ -4,8 +4,9 @@ import java.util.*
 
 /* Part 1 */
 
-fun part1(input: List<String>): Int {
-    val fields = parse(input)
+fun part1(input: List<String>): Int = solve(parse(input))
+
+fun solve(fields: List<List<Field>>): Int {
     val pq = PriorityQueue<Path>(compareBy { it.score })
     val visited = mutableSetOf<Visit>()
     pq.add(Path(end = fields[0][0]))
@@ -36,16 +37,20 @@ private fun Dir.opposite(): Dir = when (this) {
     Dir.RIGHT -> Dir.LEFT
 }
 
-data class Path(var score: Int = 0, var end: Field, var history: List<Dir> = listOf()) {
+data class Path(var score: Int = 0, var end: Field, var history: List<Dir> = listOf(), val historyLength: Int = 3) {
     fun add(field: Field): Path {
         score += field.value
-        history = (history + end.dir(field)).takeLast(3)
+        history = if (history.isEmpty() || end.dir(field) != history.last()) {
+            listOf(end.dir(field))
+        } else {
+            (history + end.dir(field)).takeLast(historyLength)
+        }
         end = field
         return this
     }
 }
 
-private fun Path.clone(): Path = Path(score, end, history.toList())
+private fun Path.clone(): Path = Path(score, end, history.toList(), historyLength)
 
 private fun Path.nextNeighbours(fields: List<List<Field>>, history: List<Dir>): List<Field> {
     val (y, x) = end
@@ -63,4 +68,31 @@ private fun Field.dir(to: Field): Dir = when {
     x > to.x -> Dir.LEFT
     x < to.x -> Dir.RIGHT
     else -> throw IllegalArgumentException()
+}
+
+/* Part 2 */
+
+fun part2(input: List<String>): Int {
+    val fields = parse(input)
+    val pq = PriorityQueue<Path>(compareBy { it.score })
+    val visited = mutableSetOf<Visit>()
+    pq.add(Path(end = fields[0][0], historyLength = 10))
+    while (pq.peek().end != fields.last().last()) {
+        val path = pq.poll()
+        if (Visit(path.end, path.history) in visited) continue
+        visited.add(Visit(path.end, path.history))
+        path.nextNeighbours2(fields, path.history).forEach { next -> pq.add(path.clone().add(next)) }
+    }
+    return pq.poll().score
+}
+
+private fun Path.nextNeighbours2(fields: List<List<Field>>, history: List<Dir>): List<Field> {
+    val (y, x) = end
+    var dirs = Dir.entries.filter { history.isEmpty() || it != history.last().opposite() }
+    if (history.size < 4) {
+        dirs = dirs.filter { history.isEmpty() || it == history.last() }
+    } else if (history.size == 10 && history.all { it == history.first() }) {
+        dirs = dirs.filter { history.isEmpty() || it != history.last() }
+    }
+    return dirs.mapNotNull { fields.getOrNull(y + it.y)?.getOrNull(x + it.x) }
 }
